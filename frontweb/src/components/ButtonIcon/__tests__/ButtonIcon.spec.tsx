651c73cb0be72175902cfeee0a6a579c881c224a
Eliton Cruvinel <eliton@netscape.net>
2023-04-27 18:14:36 -0300

    Pagination render test

--- frontweb/src/components/ButtonIcon/__tests__/ButtonIcon.spec.tsx
+++ frontweb/src/components/ButtonIcon/__tests__/ButtonIcon.spec.tsx
@@ -12,4 +12,4 @@ test('ButtonIcon should render button with given text', () => {
 
     expect(screen.getByText(text)).toBeInTheDocument();
     expect(screen.getByTestId("arrow")).toBeInTheDocument();
-})
+});
